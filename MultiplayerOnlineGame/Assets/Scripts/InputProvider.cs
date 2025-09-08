877b36df6a3f3de4e47c2f16146c93aa4dc30a09
Shlmp <daniel.kil@uniat.edu.mx>
2025-09-08 09:59:58 -0600

    Add Feature: Player Input
    Added Player Input & PlayerMovementHard

---
+++ MultiplayerOnlineGame/Assets/Scripts/InputProvider.cs
@@ -0,0 +1,162 @@
+using Fusion;
+using Fusion.Sockets;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputProvider : SimulationBehaviour, INetworkRunnerCallbacks
+{
+    private InputActions _playerActionMap;
+    private bool done;
+
+    private void Start()
+    {
+        if (Runner != null)
+        {
+            Initialized();
+        }
+    }
+
+    private void Update()
+    {
+        if (done == false && Runner != null)
+        {
+            Initialized();
+        }
+    }
+
+    private void OnDisable()
+    {
+        Runner.RemoveCallbacks(this);
+    }
+
+    private void Initialized()
+    {
+        _playerActionMap = new InputActions();
+        _playerActionMap.Player.Enable();
+        Runner.AddCallbacks(this);
+        done = true;
+    }
+
+    private void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        var myInput = new MyInput();
+        var playerActions = _playerActionMap.Player;
+        myInput.buttons.Set(MyButtons.Forward, playerActions.Move.ReadValue<Vector2>().y > 0);
+        myInput.buttons.Set(MyButtons.Backward, playerActions.Move.ReadValue<Vector2>().y < 0);
+        myInput.buttons.Set(MyButtons.Left, playerActions.Move.ReadValue<Vector2>().x > 0);
+        myInput.buttons.Set(MyButtons.Right, playerActions.Move.ReadValue<Vector2>().x < 0);
+
+        input.Set(myInput);
+    }
+
+    /*-------------------------------------------------------------------IGNORE THIS------------------------------------------------------------------------------------------*/
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+        throw new NotImplementedException();
+    }
+
+    void INetworkRunnerCallbacks.OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        OnInput(runner, input);
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public struct MyInput: INetworkInput
+{
+    public NetworkButtons buttons;
+    public Vector3 aimDirection;
+}
+
+enum MyButtons
+{
+    Forward = 0,
+    Backward = 1,
+    Left = 2,
+    Right = 3
+}
--- MultiplayerOnlineGame/Assets/Scripts/PlayerMovement.cs
+++ MultiplayerOnlineGame/Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,27 @@
 using Fusion;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : NetworkBehaviour
 {
     private CharacterController characterController;
     public float playerSpeed = 2f;
+    public InputActions inputActions;
 
     private void Awake()
     {
+        inputActions = new InputActions();
+        inputActions.Player.Enable();
         gameObject.TryGetComponent(out characterController);
     }
 
     public override void FixedUpdateNetwork()
     {
-        Vector3 move = new Vector3(0,0,0) * Runner.DeltaTime * playerSpeed;
-        /**/
-        characterController.Move(move);
+        InputSystem.Update();
+        Vector2 moveInput = inputActions.Player.Move.ReadValue<Vector2>();
+        Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
+
+        characterController.Move(move * Runner.DeltaTime * playerSpeed);
 
         if(move != Vector3.zero)
         {
